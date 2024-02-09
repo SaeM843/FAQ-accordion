dd8d2a51f3833a6c7cf4c1051df31476d58994ac
Sae Matsuda <sae.matsuda.843@gmail.com>
2024-02-09 18:08:26 +0900

    managed to change icons when it opens/closes

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 const questions = document.querySelectorAll(".question");
 const answer = document.querySelector(".hidden-answer");
+const icons = document.querySelectorAll(".plus-icons");
 
 function showAnswers(e) {
-  console.log(e.target);
+  console.log(this.children[0].src);
+  //console.log(this.nextElementSibling);
   this.nextElementSibling.classList.toggle("active");
+
+  if (this.nextElementSibling.classList.contains("active")) {
+    this.children[0].src = "./assets/images/icon-minus.svg";
+  } else {
+    this.children[0].src = "./assets/images/icon-plus.svg";
+  }
 }
 
 questions.forEach((question) =>
