7b8fd74dcf5a9b1b6e547585b745fcd8263a10d1
Sae Matsuda <sae.matsuda.843@gmail.com>
2024-03-03 12:31:07 +0900

    fixed js

--- main.js
+++ main.js
@@ -2,28 +2,17 @@
 // const answer = document.querySelector(".hidden-answer");
 // const icons = document.querySelectorAll(".plus-icons");
 const questions = document.querySelectorAll("summary");
-
-// function showAnswers(e) {
-//   console.log(this.children[0].src);
-//   //console.log(this.nextElementSibling);
-//   this.nextElementSibling.classList.toggle("active");
-
-//   if (this.nextElementSibling.classList.contains("active")) {
-//     this.children[0].src = "./assets/images/icon-minus.svg";
-//   } else {
-//     this.children[0].src = "./assets/images/icon-plus.svg";
-//   }
-// }
+const summaryInner = document.querySelectorAll(".summary-inner");
 
 function toggleIcon() {
   // console.log(this.nextElementSibling);
   // console.log(summary.parentElement);
 
-  console.log(this);
+  console.log(this.children[0].children[1]);
   if (this.parentElement.hasAttribute("open")) {
-    this.children[1].src = "assets/images/icon-plus.svg";
+    this.children[0].children[1].src = "assets/images/icon-plus.svg";
   } else {
-    this.children[1].src = "assets/images/icon-minus.svg";
+    this.children[0].children[1].src = "assets/images/icon-minus.svg";
   }
 }
 
